af1ef539381e1fb6efb392a6e2c94dd0a8081c56
Cyteen <cyteen@ring-zero.co.uk>
2017-11-01 14:18:14 +0000

    Added update initramfs.

--- 020_bootchart.sh
+++ 020_bootchart.sh
@@ -3,3 +3,5 @@ apt-get install -y bootchart2 pybootchartgui
 sed -i "s|^GRUB_CMDLINE_LINUX=\"\(.*\)\"|GRUB_CMDLINE_LINUX=\"\1 initcall_debug printk.time=y rdinitrd=/sbin/bootchartd\"|" /etc/default/grub
 
 update-grub
+
+update-mkinitramfs -k all -u
