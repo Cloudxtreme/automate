237a1fe1ae02221242df00238e9df13686b8a9c8
Cyteen <cyteen@ring-zero.co.uk>
2017-11-03 20:40:52 +0000

    Removed qupzilla browser.

--- 010_apt-get.sh
+++ 010_apt-get.sh
@@ -26,7 +26,6 @@ apt-get -y install ntp ntpdate
 apt-get -y install gstreamer1.0-plugins-ugly gstreamer1.0-plugins-good gstreamer1.0-libav
 apt-get -y install libreoffice-pdfimport 
 apt-get -y install terminology
-apt-get -y install qupzilla
 apt-get -y install firmware-iwlwifi #For thinkpad wifi: Intel 5100 AGN [Shiloh] 
 apt-get -y install grub-pc grub2 gpart
 apt-get install -y vim-gtk software-properties-common apt-transport-https wget curl lvm2
