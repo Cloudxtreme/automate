7ad78c88c71ce8c157708110eb11a8b47b2b68e3
Cyteen <cyteen@ring-zero.co.uk>
2017-11-03 17:10:49 +0000

    Removed backtick expansion.

--- 001_docker_grub-commandline.sh
+++ 001_docker_grub-commandline.sh
@@ -1,5 +1,5 @@
 ## Docker likes kernel swappiness support (on reboot)
-bash -c `perl -p -i -e 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="cgroup_enable=memory swapaccount=1"/g'  /etc/default/grub`
+bash -c "$(perl -p -i -e 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="cgroup_enable=memory swapaccount=1"/g'  /etc/default/grub)"
 
 #Now that your server supports swap limit capabilities in your docker run command you can use --memory-swappiness=0 and set --memory-swap equal to --memory. You also need to set -Des.bootstrap.mlockall=true on the docker run commandline.
 
