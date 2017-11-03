43232d98e1685d2470c54b5997cbe4e5e7baae8d
Cyteen <cyteen@ring-zero.co.uk>
2017-11-03 17:14:02 +0000

    Removed backtick expansion and fixed server name.

--- sbuild-devuan.sh
+++ sbuild-devuan.sh
@@ -1,14 +1,15 @@
+#!/bin/bash
 LOGNAME=${USER}
 RELEASE=$(cat /etc/devuan_version)
 KEYRING=/usr/share/keyrings/devuan-archive-keyring.gpg
 INCLUDE="eatmydata,ccache,gnupg"
-MIRROR=http://auto.mirror.devuan.org/merged
+MIRROR=http://pkgmaster.devuan.org/merged
 
 apt-get install -y sbuild
 mkdir -p /root/.gnupg # To work around #792100
 sbuild-update --keygen # see #801798
 sbuild-adduser ${LOGNAME}
 usermod -g sbuild ${LOGNAME}
-`newgrp sbuild`
+$(newgrp sbuild)
 
 sbuild-createchroot --keyring=${KEYRING} --include=${INCLUDE} ${RELEASE} /srv/chroot/${RELEASE}-amd64-sbuild ${MIRROR}
