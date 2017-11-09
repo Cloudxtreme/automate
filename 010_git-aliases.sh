23411be7191c1971ef179a616ec5123916108604
Cyteen <cyteen@ring-zero.co.uk>
2017-11-09 16:46:29 +0000

    Added alias for git up to avoid git pull.

--- 010_git-aliases.sh
+++ 010_git-aliases.sh
@@ -3,3 +3,6 @@
 
 # git log --oneline --abbrev-commit --all --graph --decorate --color --date=relative --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'
 git config --global alias.lg "log --oneline --abbrev-commit --all --graph --decorate --color --date=relative --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'"
+
+# see the problems with git pull: https://stackoverflow.com/questions/15316601/in-what-cases-could-git-pull-be-harmful/15316602#15316602
+git config --global alias.up '!git remote update -p; git merge --ff-only @{u}'
