25d6cbab93c10a7973b1c799bbd640bf45b38803
Cyteen <cyteen@ring-zero.co.uk>
2017-11-09 16:54:41 +0000

    As the hard depends on avahi spreads we have to be a little less complete in out removal, until we push hard depends into suggests or recommends.

--- 050_remove_avahi.sh
+++ 050_remove_avahi.sh
@@ -1,6 +1,8 @@
 # removing any more of avahi need rebuilding of libpurple libgnomevfs libgnome2
 bash -c "update-rc.d avahi-daemon disable"
-apt-get -y remove --purge cups-browsed
-apt-get -y remove --purge avahi-autoipd libavahi-common-data libavahi-common3 avahi-daemon libavahi-glib1 libavahi-client3
+apt-get -y remove --purge cups-browsed avahi-autoipd
+
+echo " This stops the avahi daemon but the dependencies of any of the libavahi reach throught to just about all desktop packages."
+#apt-get -y remove --purge libavahi-common-data libavahi-common3 avahi-daemon libavahi-glib1 libavahi-client3
 
 
