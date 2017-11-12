c3cdeb577c9aa9c975b111bf133799eeef30eb6a
Cyteen <cyteen@ring-zero.co.uk>
2017-11-12 15:16:58 +0000

    Added generation on british locale.

--- 001_locales.sh
+++ 001_locales.sh
@@ -1,6 +1,6 @@
 # Set env
 apt-get -y install locales
-
+locale-gen "en_GB.UTF-8"
 export MYLANGUAGE="en_GB.UTF-8"
 export MYLANG="en_GB.UTF-8"
 export MYLC_ALL="en_GB.UTF-8"
@@ -10,7 +10,7 @@ sed -i -e "s/# ${MYLANG}\ UTF-8/${MYLANG}\ UTF-8/" /etc/locale.gen
 update-locale LANG=${MYLANG}
 dpkg-reconfigure --frontend=noninteractive locales
 
-export LANGUAGE="${MYLANGUAGE}"
-export LANG="${MYLANG}"
-export LC_ALL="${MYLC_ALL}"
+#export LANGUAGE="${MYLANGUAGE}"
+#export LANG="${MYLANG}"
+#export LC_ALL="${MYLC_ALL}"
 
