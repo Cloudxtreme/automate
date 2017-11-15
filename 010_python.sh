d8053e1d910a8f0b17e92c50662b4b93d944c2cb
Cyteen <cyteen@ring-zero.co.uk>
2017-11-15 18:25:54 +0000

    A central place to add expected python libraries.

---
+++ 010_python.sh
@@ -0,0 +1,7 @@
+apt-get install -y python-dev python3-dev
+apt-get install -y python3-requests
+apt-get install -y python3-autopep8
+apt-get install -y python3-ipython
+apt-get install -y python3-pylint
+apt-get install -y virtualenvwrapper
+apt-get install -y prospector
