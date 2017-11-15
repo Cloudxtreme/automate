ce8ec359737e6422013e193e43bb1af84b61af3e
Cyteen <cyteen@ring-zero.co.uk>
2017-11-15 19:51:01 +0000

    Fix destination for sources.list.

--- 020_waterfox.sh
+++ 020_waterfox.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
+set -e
 mkdir -p /etc/apt//sources.list-available || exit
-cat > /etc/apt/sources.list-available << EOF
+cat > /etc/apt/sources.list-available/waterfox.list << EOF
 # curl https://bintray.com/user/downloadSubjectPublicKey\?username\=hawkeye116477 | sudo apt-key add -
 deb https://dl.bintray.com/hawkeye116477/waterfox-deb release main
 EOF
 
-ln -s /etc/apt/sources.list-available/waterfox.list /etc/apt/sources.list.d/waterfox.list
+ln -sf /etc/apt/sources.list-available/waterfox.list /etc/apt/sources.list.d/waterfox.list
 
 curl https://bintray.com/user/downloadSubjectPublicKey\?username\=hawkeye116477 | sudo apt-key add -
 
