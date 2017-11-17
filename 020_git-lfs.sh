04b575bd5bebad384753158ae813f0901dfe09af
Cyteen <cyteen@ring-zero.co.uk>
2017-11-17 17:58:33 +0000

    Add some pointers, links to the end of the file.

--- 020_git-lfs.sh
+++ 020_git-lfs.sh
@@ -41,3 +41,24 @@ apt-get update
 
 apt-get install -y git-lfs
 apt-get -f install -y
+
+
+echo "See: http://github.com/git-lfs/git-lfs/wiki/Tutorial"
+
+echo "Per user: 
+	git lfs install"
+echo "Per repo: 
+	git lfs install --local"
+echo "Use --skip-smudge to prevent automatic download on clone/pull"
+echo "Add pattern to track with:"
+echo "	git lfs track '*.tar.xz'"
+echo "and add the resulting .gitattribute to the repo with:"
+echo "	git add .gitattrubute"
+echo "To see the LFS url:"
+echo "	git lfs env | grep 'Endpoint='"
+echo "To set it:"
+echo "	git config -f .lfsconfig lfs.url https://my_other_server.example.com/foo/bar/info/lfs'"
+echo "	git add .lfsconfig"
+echo "For servers see:"
+echo "	https://docs.gitlab.com/ce/workflow/lfs/manage_large_binaries_with_git_lfs.html"
+echo "	https://about.gitlab.com/2017/01/30/getting-started-with-git-lfs-tutorial/"
