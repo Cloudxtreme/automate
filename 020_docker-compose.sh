1234379b9f302af0a497969a87a2e29b48c1d645
Cyteen <cyteen@ring-zero.co.uk>
2017-11-27 21:36:03 +0000

    Correct rest api calls and remove cruft.

--- 020_docker-compose.sh
+++ 020_docker-compose.sh
@@ -1,23 +1,38 @@
+#!/usr/bin/env bash
 #dpkg -i /var/tmp/docker-compose/docker-compose_1.11.0-1_amd64.deb 
 
-LATEST_URL=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep browser_download_url | cut -d '"' -f 4 | grep docker-compose-$(uname -s)-$(uname -m))
-RELEASE=$(echo ${LATEST_URL} | awk '{split($0,a,"/"); print a[8]}')
+BASE_URL='https://api.github.com/repos'
+BASE_USER='docker'
+BASE_REPO='compose'
+RELEASES_URL="${BASE_URL}/${BASE_USER}/${BASE_REPO}"/releases
+CONTENT=$(curl -s ${RELEASES_URL}/latest)
+RELEASE=$(echo "${CONTENT}" | jq --raw-output '.tag_name')
 
 # make a containing directory
-mkdir -p /var/tmp/docker-compose-${RELEASE}
-cd /var/tmp/docker-compose-${RELEASE}
+mkdir -p /var/tmp/docker-compose-"${RELEASE}"
+cd /var/tmp/docker-compose-"${RELEASE}" || exit
 
 cat > ./description-pak << EOF
 Compose is a tool for defining and running multi-container Docker applications. With Compose, you use a Compose file to configure your application’s services. Then, using a single command, you create and start all the services from your configuration.
 EOF
 
-cat > ./checkinstall_it.sh << EOF
-set +x
-# 
+cat > ./checkinstall_it.sh << 'EOF'
 
-LATEST_URL=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep browser_download_url | cut -d '"' -f 4 | grep docker-compose-$(uname -s)-$(uname -m))
-RELEASE=$(echo ${LATEST_URL} | awk '{split($0,a,"/"); print a[8]}')
-LICENSE_URL='https://raw.githubusercontent.com/docker/compose/master/LICENSE'
+echo "ENTERING CHECKINSTALL"
+
+BASE_URL='https://api.github.com/repos'
+BASE_USER='docker'
+BASE_REPO='compose'
+LICENSE_PAGE_URL="${BASE_URL}/${BASE_USER}/${BASE_REPO}"/license
+RELEASES_URL="${BASE_URL}/${BASE_USER}/${BASE_REPO}"/releases
+CONTENT=$(curl -s ${RELEASES_URL}/latest)
+LATEST_URL=$(echo "${CONTENT}" | jq --raw-output '.assets[] | select(.browser_download_url | test("Linux-x86_64")) | .browser_download_url')
+
+echo "LATEST_URL: " "${LATEST_URL}"
+
+RELEASE=$(echo "${CONTENT}" | jq --raw-output '.tag_name')
+
+LICENSE_URL=$(curl "${LICENSE_PAGE_URL}" | jq --raw-output '.download_url')
 wget -c ${LICENSE_URL}
 
 # make a new temporary directory for this use
@@ -39,10 +54,16 @@ checkinstall -y --fstrans \
 	bash ./install.sh
 EOF
 
-cat > ./install.sh << EOF
-set +x
+cat > ./install.sh << 'EOF'
+BASE_URL='https://api.github.com/repos'
+BASE_USER='docker'
+BASE_REPO='compose'
+RELEASES_URL="${BASE_URL}/${BASE_USER}/${BASE_REPO}"/releases
+CONTENT=$(curl -s ${RELEASES_URL}/latest)
+LATEST_URL=$(echo "${CONTENT}" | jq --raw-output '.assets[] | select(.browser_download_url | test("Linux-x86_64")) | .browser_download_url')
+
 # Download and copy the latest version to /usr/local/bin and make executable
-curl -o /usr/local/bin/docker-compose -L https://api.github.com/repos/docker/compose/releases/latest && chmod +x /usr/local/bin/docker-compose
+curl -s -o /usr/local/bin/docker-compose -L ${LATEST_URL} && chmod +x /usr/local/bin/docker-compose
 EOF
 
 bash ./checkinstall_it.sh
